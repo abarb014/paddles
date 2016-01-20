79e27dff410340486168240496096ab6c24bffdd
Andrew Barboza <abarb014@ucr.edu>
2016-01-19 17:17:43 -0800

    added background image

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
+#include <iostream>
+#include <cstdlib>
 #include <SFML/Graphics.hpp>
 
 int main(int argc, char *argv[])
 {
     sf::RenderWindow window(sf::VideoMode(640, 480), "Paddles");
     sf::Event event;
+
+    // just making sure I do something every day
+    sf::Texture bgImage;
+    if (!bgImage.loadFromFile("assets/images/paddlesBg.png"))
+    {
+        std::cerr << "[Error]: Unable to load background image" << std::endl;
+        exit(-1);
+    }
+    sf::Sprite background;
+    background.setTexture(bgImage);
 
     // main game loop
     while (window.isOpen())
@@ -15,6 +27,7 @@ int main(int argc, char *argv[])
         }
 
         window.clear();
+        window.draw(background);
         window.display();
     }
 }
